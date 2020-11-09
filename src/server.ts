a4acacebab002bd45a38a4bd026c46ce2b5a0bba
Deby Silvia Agnes <deby.silvia23@gmail.com>
2020-11-09 18:54:41 +0700

    Edit (#6)
    * initial commit
    
    * feat(database): setup database connection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 import app from './app'
+import { connectDB } from './utils/database'
 
 const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(`listening on http://localhost:${port}/`)
-})
+
+connectDB()
+  .then(() => {
+    console.log('Database connected!')
+    app.listen(port, () => {
+      console.log(`Product-service listening on http://localhost:${port}/`)
+    })
+  })
+  .catch((err: string) => {
+    console.log(err)
+  })
